487a7b3ff1a122e83af0c02a136c50a4f0da9587
AtsukiTak <takatomgoo@gmail.com>
2022-02-09 15:51:31 +0900

    run-qemu.shにNOGRAPHICモードを追加

--- bootloader/run-qemu.sh
+++ bootloader/run-qemu.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
 cp $1 mnt/EFI/BOOT/BOOTx64.EFI
-qemu-system-x86_64 -bios OVMF.fd -drive format=raw,file=fat:rw:mnt
+
+ARGS="-bios OVMF.fd"
+ARGS="-drive format=raw,file=fat:rw:mnt ${ARGS}"
+
+if [ "$NOGRAPHIC" = "true" ]
+then
+  ARGS="-nographic ${ARGS}"
+fi
+
+echo $ARGS
+
+qemu-system-x86_64 ${ARGS}
