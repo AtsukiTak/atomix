515bc96e6e554c62aee2857130574ce47d638806
AtsukiTak <takatomgoo@gmail.com>
2022-03-24 17:33:41 +0900

    OVMFの使用方法を修正

--- bootloader/run-qemu.sh
+++ bootloader/run-qemu.sh
@@ -2,8 +2,9 @@
 
 cp $1 mnt/EFI/BOOT/BOOTx64.EFI
 
-ARGS="-bios OVMF.fd"
-ARGS="-drive format=raw,file=fat:rw:mnt ${ARGS}"
+# ARGS="-bios OVMF.fd"
+SRCDIR="${HOME}/Develop/atsuki-tak/atomix/bootloader"
+ARGS="-drive if=pflash,format=raw,readonly=on,file=${SRCDIR}/OVMF_CODE.fd -drive if=pflash,format=raw,file=${SRCDIR}/OVMF_VARS.fd -drive format=raw,file=fat:rw:mnt"
 
 if [ "$NOGRAPHIC" = "true" ]
 then
